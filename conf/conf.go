077819bb80ba5daf0ac2cb6551851c09b67dd444
bjke <kbjwdh@gmail.com>
2023-07-12 23:50:55 +0800

    add env

--- conf/conf.go
+++ conf/conf.go
@@ -1,10 +1,7 @@
 package conf
 
 import (
-	"os"
 	"strings"
-
-	"github.com/pelletier/go-toml/v2"
 )
 
 type ConfigStruct struct {
@@ -41,33 +38,39 @@ var Conf ConfigStruct
 var Models ModelsResp
 
 func Setup() {
-	v, err := os.ReadFile("config.toml")
-	if err != nil {
-		panic(err)
-	}
-	err = toml.Unmarshal(v, &Conf)
-	if err != nil {
-		panic(err)
-	}
+	//v, err := os.ReadFile("config.toml")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//err = toml.Unmarshal(v, &Conf)
+	//if err != nil {
+	//	panic(err)
+	//}
 	if Conf.Port == 0 {
-		Conf.Port = 3700
+		Conf.Port = 8080
 	}
 	if Conf.RateLimit == 0 {
 		Conf.RateLimit = 10
 	}
 	if Conf.Bot == nil {
 		Conf.Bot = map[string]string{
-			"gpt-3.5-turbo":       "chinchilla",
-			"gpt-4":               "beaver",
-			"gpt-3.5-turbo-0301":  "a2",
-			"gpt-4-32k":           "a2_100k",
-			"gpt-4-0314":          "a2_2",
-			"Sage":                "capybara",
-			"ChatGPT":             "chinchilla",
-			"GPT-4":               "beaver",
-			"Claude-instant":      "a2",
-			"Claude-instant-100k": "a2_100k",
-			"Claude+":             "a2_2",
+			"gpt-3.5-turbo":           "chinchilla",
+			"gpt-4":                   "beaver",
+			"gpt-3.5-turbo-0301":      "a2",
+			"gpt-4-32k":               "a2_100k",
+			"gpt-4-0314":              "a2_2",
+			"gpt-Sage":                "capybara",
+			"gpt-ChatGPT":             "chinchilla",
+			"gpt-GPT-4":               "beaver",
+			"gpt-Claude-instant":      "a2",
+			"gpt-Claude-instant-100k": "a2_100k",
+			"gpt-Claude+":             "a2_2",
+			"Sage":                    "capybara",
+			"ChatGPT":                 "chinchilla",
+			"GPT-4":                   "beaver",
+			"Claude-instant":          "a2",
+			"Claude-instant-100k":     "a2_100k",
+			"Claude+":                 "a2_2",
 		}
 	}
 
